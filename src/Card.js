4d54f3b66257598dd1c4fab9c6253fd263730dbd
magotts <techybet@gmail.com>
2021-11-23 23:20:36 -0500

    add location and company and username

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,17 @@ import React from "react";
 
 const Card = (props) => {
   // const profile = this.props;
+
+  console.log(props);
   return (
     <div className="github-profile">
       <img className="profile-img" src={props.avatar_url} alt="profile pic" />
       <div className="profile-info">
-        <div className="profile-name">{props.name}</div>
+        <div className="profile-name">Username: {props.login}</div>
+        <div className="profile-name">Name: {props.name}</div>
         <div className="company">{props.company}</div>
+        <div className="profile-name">{props.location}</div>
+
       </div>
     </div>
   );
