109ffbade1fb3ee691a636e48889fc458795d6e8
magotts <techybet@gmail.com>
2021-11-24 02:07:55 -0500

    add bio

--- src/Card.js
+++ src/Card.js
@@ -10,11 +10,17 @@ const Card = (props) => {
       <img className="profile-img" src={props.avatar_url} alt="profile pic" />
       <div className="profile-info">
         <div className="profile-name">Username: {props.login}</div>
-        <div className="profile-name">Name: {props.name}</div>
-        <div className="company">{props.company}</div>
-        <div className="profile-name">{props.location}</div>
-        <div className="profile-name"><a href={props.html_url} target="_blank">GitHub</a></div>
+        {props.name && ( <div className="profile-name">Name: {props.name}</div> )}
+{props.bio && (<details className="bio-summary">
+  <summary>Bio</summary>
+  <p>{props.bio}</p>
+</details>)}
 
+        {props.company && (<div className="company">Company: {props.company}</div>)}
+        {props.location && ( <div className="profile-name">Location: {props.location} </div>) }
+        {props.twitter_username && ( <div className="profile-name">Twitter: {props.twitter_username}</div>) }
+        {props.html_url && ( <div className="profile-name"><a href={props.html_url} target="_blank">GitHub</a></div> )}
+        {props.public_repos && ( <div className="profile-name">Repo Count: {props.public_repos}</div> )}
       </div>
     </div>
     </div>
